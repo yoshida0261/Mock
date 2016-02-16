10d7e2344b06c8485b3980ae45dd7c7498e8e6c9
yoshida0261 <yoshida261@gmail.com>
2016-02-17 06:06:43 +0900

    1st commit

---
+++ app/src/androidTest/java/stcompany/com/mock/ApplicationTest.java
@@ -0,0 +1,13 @@
+package stcompany.com.mock;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
